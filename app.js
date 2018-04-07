dd7522336c70fdf606369bbba19138588c43dca6
Andrew <andbostick@gmail.com>
2018-04-06 20:40:03 -0700

    Added user UI

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //init Github
 const github = new Github;
+const ui = new UI
 
 //search input
 const searchUser = document.getElementById('searchUser')
@@ -13,12 +14,14 @@ searchUser.addEventListener('keyup', (e) => {
          .then(data => {
             if(data.profile.message === 'Not Found'){
                 //show alert
-
+                ui.showAlert('User not found', 'alert alert-danger');
             } else {
                 //show profile
+                ui.showProfile(data.profile);
             }
         })
     } else {
         //clear profile
+        ui.clearProfile();
     }
 });
