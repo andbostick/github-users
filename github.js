170e24edac1933b4857532f0814102564afea7eb
Andrew <andbostick@gmail.com>
2018-04-10 19:18:36 -0700

    Finished project

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class Github {
     constructor() {
         this.client_id = '405fc9b7b65d28d9b721';
         this.client_secret = '2c43ea8e962db43953df722e7ab3d3027c802964';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
